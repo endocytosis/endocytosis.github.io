c4d0a8423e9fab1d04b8ffa5927c99362217fed1
endocytosis <brian_t.gallagher@tufts.edu>
2024-02-26 14:38:39 -0600

    Image update

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SITE: Site = {
   author: "Brian Gallagher",
   desc: "Endocytosis",
   title: "Endocytosis",
-  ogImage: "astropaper-og.jpg",
+  ogImage: "topography.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
